7f4adfffffe5101e84d2798ce75a32cf177d125a
chikhidev <chikhi.dev@gmail.com>
2023-06-17 00:19:51 +0100

    LEET_LOW_LEVEL - ../.vscode/settings.json main.c main.exe test ../70_Climbing_Stairs/main.exe ../70_Climbing_Stairs/test ../push.sh ../.vscode/tasks.json ../push

--- 509_Fibonacci_Number/main.c
+++ 509_Fibonacci_Number/main.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 int main(){
-    int goal = 10;
     
-    while (goal >= 0){
-        printf("%d\n", fib(goal));
-        goal--;
-    }
+    printf("%d\n", fib(10));
+    printf("%d\n", fib(9));
+    printf("%d\n", fib(8));
+    printf("%d\n", fib(7));
+    printf("%d\n", fib(2));
+    printf("%d\n", fib(1));
+    printf("%d\n", fib(0));
 
     return 0;
 }
