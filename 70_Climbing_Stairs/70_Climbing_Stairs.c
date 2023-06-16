c6cf0149f18c40ad3283b64b690aa305e0af40d4
chikhidev <chikhi.dev@gmail.com>
2023-06-16 22:33:18 +0100

    LEET_LOW_LEVEL - ../.vscode/settings.json 70_Climbing_Stairs.c main.c main.exe ../.vscode/tasks.json

--- 70_Climbing_Stairs/70_Climbing_Stairs.c
+++ 70_Climbing_Stairs/70_Climbing_Stairs.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
-int power_of(int n, int pow){
+int power_of(int pow){
     int res;
     
     if (pow == 0) return 1;
-    if (pow == 1) return n;
+    if (pow == 1) return 2;
 
     res = 0;
-    while (pow-- > 0)
-        res *= n;
+    while (pow > 1){
+        res *= 2;
+        pow--;
+    }
     return res;
 }
 
 int climbStairs(int n){
     int power = 0;
-    if (n == (1 || 0)) return n;
+    if (n == 1 || n == 0) return n;
     if (n >= 2)
         power = n - 2;
-    return power_of(2 , power) + 1;
+    return power_of(power) + 1;
 }
--- 70_Climbing_Stairs/main.c
+++ 70_Climbing_Stairs/main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 int main(){
-    int res;
-    res = climbStairs(3);
-    printf("%d\n", res);
-    printf("%d\n", (2^0) + 1);
+    printf("0:\t%d\n", climbStairs(0));
+    printf("1:\t%d\n", climbStairs(1));
+    printf("2:\t%d\n", climbStairs(2));
+    printf("3:\t%d\n", climbStairs(3));
+    printf("4:\t%d\n", climbStairs(4));
     return 0;
 }
