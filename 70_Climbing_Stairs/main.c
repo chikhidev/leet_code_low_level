563b53473c3f29aae5c11dea4b7b9bbb34a7e511
chikhidev <chikhi.dev@gmail.com>
2023-06-16 17:03:52 +0100

    LEET_LOW_LEVEL - 70_Climbing_Stairs/main.c push.sh

---
+++ 70_Climbing_Stairs/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(){
+    int res;
+    res = climbStairs(2);
+    printf("%d", res);
+    return 0;
+}
--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ git add .
 if [[ -n $(git status -s) ]]; then
   file_names=$(git status -s | awk '{print $2}')
 
-  commit_message="LEET_LOW_LEVEL - Files changed: $file_names"
+  commit_message="LEET_LOW_LEVEL - $file_names"
 
   git commit -m "$commit_message"
 
