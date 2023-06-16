74971f761339d5332e9df0efecd3d43684d8f81e
chikhidev <chikhi.dev@gmail.com>
2023-06-16 17:16:55 +0100

    LEET_LOW_LEVEL - ../.vscode/settings.json 70_Climbing_Stairs.c main.c main.exe

--- 70_Climbing_Stairs/70_Climbing_Stairs.c
+++ 70_Climbing_Stairs/70_Climbing_Stairs.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
+int power_of(int n, int pow){
+    int res;
+    
+    if (pow == 0) return 1;
+    if (pow == 1) return n;
+
+    res = 0;
+    while (pow-- > 0)
+        res *= n;
+    return res;
+}
+
 int climbStairs(int n){
     int power = 0, factor = 0;
 
     if (n >= 2)
         power = n - 2;
-    factor = (2 ^ factor) + 1;
+    factor = power_of(2 , factor) + 1;
 
     return factor;
 }
--- 70_Climbing_Stairs/main.c
+++ 70_Climbing_Stairs/main.c
@@ -3,6 +3,7 @@
 int main(){
     int res;
     res = climbStairs(2);
-    printf("%d", res);
+    printf("%d\n", res);
+    printf("%d\n", (2^0) + 1);
     return 0;
 }
