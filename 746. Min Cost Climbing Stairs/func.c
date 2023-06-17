45ba38b4325f230a79018dced2bc14ee77005b25
chikhidev <chikhi.dev@gmail.com>
2023-06-17 23:27:32 +0100

    LEET_LOW_LEVEL - ../.vscode/settings.json "../1137. func.c main.exe ../test

--- 746. Min Cost Climbing Stairs/func.c
+++ 746. Min Cost Climbing Stairs/func.c
@@ -1,13 +1,35 @@
 #include "main.h"
 
 //[1,100,1,1,1,100,1,1,100,1]
+//[10,1,1,1,1,100,1,1,100,1]
 
 int minCostClimbingStairs(int* cost, int costSize){
     if(costSize == 0) return 0;
-    int i = 1;
-    while (i < costSize){
-        printf("%d\n", cost[i]);
-        i++;
+    int i, res = 0;
+
+    if (cost[0] < cost[1]){
+        i = 0;
+        res += cost[0];
     }
-    return 0;
+    else{
+        i = 1;
+        res += cost[1];
+    };
+
+    while (i < costSize){
+        if(cost[i + 1] && cost[i + 2]){
+            if (cost[i + 1] < cost[i + 2]){
+                res += cost[i + 1];
+            }
+            else{
+                i++;
+                res += cost[i + 2];
+            }
+        }else{
+            res += cost[i + 1];
+        }
+        i++;
+
+    }
+    return res;
 }
