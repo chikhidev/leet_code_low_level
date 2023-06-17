ae634a44b6fb62031989e45a007c04b6fcdb4ee8
chikhidev <chikhi.dev@gmail.com>
2023-06-17 12:25:59 +0100

    LEET_LOW_LEVEL - func.c main.exe ../test

--- 1137. N-th Tribonacci Number/func.c
+++ 1137. N-th Tribonacci Number/func.c
@@ -4,11 +4,11 @@ int tribonacci(int n){
     if (n==0) return 0;
     
     int *FAB = (int*)malloc((n + 1) * sizeof(int));
-    int target, i = 0;
+    int target, i = 3;
 
     FAB[0] = 0;
     FAB[1] = FAB[2] = 1;
-    while (i < (n + 1)){
+    while (i <= n){
         *(FAB + i) = *(FAB + i - 1) + *(FAB + i - 2);
         printf("%d\n", *(FAB + i));
         i++;
