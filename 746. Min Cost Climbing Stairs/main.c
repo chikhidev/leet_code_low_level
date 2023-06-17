7b9d6a63f6bc741e6a221da469fc481c7bfdf817
chikhidev <chikhi.dev@gmail.com>
2023-06-17 23:39:52 +0100

    LEET_LOW_LEVEL - ../.vscode/settings.json "../1137. main.c main.exe ../test

--- 746. Min Cost Climbing Stairs/main.c
+++ 746. Min Cost Climbing Stairs/main.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
-int main(){
-    int *cost[10] = {
-        1,100,1,1,1,100,1,1,100,1
-    };
-    printf("min: %d", minCostClimbingStairs(cost, 10));
-    
+int main() {
+    int cost1[] = {10, 15, 20};
+    int costSize1 = sizeof(cost1) / sizeof(cost1[0]);
+    int result1 = minCostClimbingStairs(&cost1, costSize1);
+    printf("Minimum cost for cost1: %d\n", result1);
+
+    int cost2[] = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+    int costSize2 = sizeof(cost2) / sizeof(cost2[0]);
+    int result2 = minCostClimbingStairs(&cost2, costSize2);
+    printf("Minimum cost for cost2: %d\n", result2);
+
     return 0;
 }
